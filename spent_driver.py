da449712a948189930849865901d0d9663876049
Arna-Maity <arnamaity6230@gmail.com>
2020-01-06 21:30:19 +0530

    Added the user profile changes to Community Version and reverted the original files.

--- spent_driver.py
+++ spent_driver.py
@@ -1,13 +1,9 @@
-#spent_driver.py use [<profile_name>]
 usage = '''
-
 Expense Tracker CLI.
-
 Usage:
   spent_driver.py init
-  spent_driver.py view [<view_profile>] [<view_category>] 
-  spent_driver.py <amount> <category> [<profile_name>] [<message>] 
-
+  spent_driver.py view [<view_category>]
+  spent_driver.py <amount> <category> [<message>]
 '''
 
 from docopt import docopt
@@ -15,7 +11,6 @@ from spent import *
 from tabulate import tabulate
 
 args = docopt(usage)
-#print(args)
 
 if args['init']:
     init()
@@ -23,24 +18,13 @@ if args['init']:
 
 if args['view']:
     category = args['<view_category>']
-    profile = args['<view_profile>']
-    if profile:
-        amount, results = view(category, profile)
-
-    else:
-        amount, results = view(category)
+    amount, results = view(category)
     print("Total expense: ", amount)
     print(tabulate(results))
 
 if args['<amount>']:
     try:
         amount = float(args['<amount>'])
-        if(args['<profile_name>']):
-            log(amount, args['<category>'],args['<message>'],args['<profile_name>'])
-        else:
-            log(amount, args['<category>'],args['<message>'])
-
-
+        log(amount, args['<category>'], args['<message>'])
     except:
         print(usage)
-
